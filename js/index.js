563a3bea42b01307258f2f06405b8cbe052cefc6
Nikita S <->
2023-01-10 17:58:23 +0300

    разработка формы на JS

---
+++ js/index.js
@@ -0,0 +1,46 @@
+let page = document.querySelector('.page');
+
+let editButton = page.querySelector('.profile__edit-button');
+
+let popup = page.querySelector('.popup');
+let closedButton = page.querySelector('.popup__close');
+
+
+function opened() {
+  page.classList.add('page_shadow');
+  popup.className = 'popup_opened';
+}
+
+editButton.addEventListener('click', opened);
+
+function closed() {
+  popup.className = 'popup';
+  page.classList.remove('page_shadow');
+}
+
+closedButton.addEventListener('click', closed);
+
+
+let formElement = page.querySelector('.popup');
+
+let nameInput = page.querySelector('.popup__input_type_name');
+let jobInput = page.querySelector('.popup__input_type_about');
+
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+
+
+    nameInput = nameInput.value;
+    jobInput = jobInput.value;
+
+    let profileName = page.querySelector('.profile__name');
+    let profileDescription = page.querySelector('.profile__description');
+
+    profileName.textContent = nameInput;
+    profileDescription.textContent = jobInput;
+
+    closed();
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
