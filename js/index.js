b4ec37a5cbef54a965805bae92f33d658ed28cfc
Nikita S <->
2023-01-12 01:56:19 +0300

    fix: оптимизировать мобильную версию сайта и привести к макету / оптимизировать структуру кода html / отчистить форму JS от лишнего кода

--- js/index.js
+++ js/index.js
@@ -2,45 +2,39 @@ let page = document.querySelector('.page');
 
 let editButton = page.querySelector('.profile__edit-button');
 
-let popup = page.querySelector('.popup');
-let closedButton = page.querySelector('.popup__close');
-
-
-function opened() {
-  page.classList.add('page_shadow');
-  popup.className = 'popup_opened';
-}
-
-editButton.addEventListener('click', opened);
-
-function closed() {
-  popup.className = 'popup';
-  page.classList.remove('page_shadow');
-}
-
-closedButton.addEventListener('click', closed);
-
-
 let formElement = page.querySelector('.popup');
+let closedButton = page.querySelector('.popup__close');
 
 let nameInput = page.querySelector('.popup__input_type_name');
 let jobInput = page.querySelector('.popup__input_type_about');
 
+let profileName = page.querySelector('.profile__name');
+let profileDescription = page.querySelector('.profile__description');
+
+function openModal() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
+  page.classList.add('page_shadow');
+  formElement.classList.add('popup_opened');
+}
+
+function closeModal() {
+  formElement.classList.remove('popup_opened');
+  page.classList.remove('page_shadow');
+}
 
 function handleFormSubmit (evt) {
-    evt.preventDefault();
+  evt.preventDefault();
+  nameNew = nameInput.value;
+  descriptionNew = jobInput.value;
 
+  profileName.textContent = nameNew;
+  profileDescription.textContent = descriptionNew;
 
-    nameNew = nameInput.value;
-    descriptionNew = jobInput.value;
-
-    let profileName = page.querySelector('.profile__name');
-    let profileDescription = page.querySelector('.profile__description');
-
-    profileName.textContent = nameNew;
-    profileDescription.textContent = descriptionNew;
-
-    closed();
+  closeModal();
 }
+
+editButton.addEventListener('click', openModal);
+closedButton.addEventListener('click', closeModal);
 
 formElement.addEventListener('submit', handleFormSubmit);
