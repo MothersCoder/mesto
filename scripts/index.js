bd2fde3642e5139e63f048ae0c59ec13ab5c7195
Nikita S <->
2023-01-24 22:28:37 +0300

    создать автоматическое выведение базовых карточек через JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,16 @@
-let page = document.querySelector('.page');
+const page = document.querySelector('.page');
 
-let editButton = page.querySelector('.profile__edit-button');
+const editButton = page.querySelector('.profile__edit-button');
 
-let popup = page.querySelector('.popup')
-let formElement = page.querySelector('.popup__form');
-let closedButton = page.querySelector('.popup__close');
+const popup = page.querySelector('.popup')
+const formElement = page.querySelector('.popup__form');
+const closedButton = page.querySelector('.popup__close');
 
-let nameInput = page.querySelector('.popup__input_type_name');
-let jobInput = page.querySelector('.popup__input_type_about');
+const nameInput = page.querySelector('.popup__input_type_name');
+const jobInput = page.querySelector('.popup__input_type_about');
 
-let profileName = page.querySelector('.profile__name');
-let profileDescription = page.querySelector('.profile__description');
+const profileName = page.querySelector('.profile__name');
+const profileDescription = page.querySelector('.profile__description');
 
 function openModal() {
   nameInput.value = profileName.textContent;
@@ -37,3 +37,41 @@ editButton.addEventListener('click', openModal);
 closedButton.addEventListener('click', closeModal);
 
 formElement.addEventListener('submit', handleFormSubmit);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const placeTamplate = document.querySelector('#place__item').content;
+const placeList = document.querySelector('.place');
+
+initialCards.forEach(function (item) {
+  const placeElement = placeTamplate.querySelector('.place__item').cloneNode(true);
+  placeElement.querySelector('.place__image').src = item.link;
+  placeElement.querySelector('.place__image').alt = item.name;
+  placeElement.querySelector('.place__title').textContent = item.name;
+  placeList.prepend(placeElement);
+});
